07fa7141483973fbae5e2b1c9cddc6dc04e7a752
Trishansh Sahane <133603023+trish-2610@users.noreply.github.com>
2025-01-01 18:31:38 +0530

    Add files via upload

---
+++ SQL_basics.sql
@@ -0,0 +1,18 @@
+-- show databases ;
+-- use practice ;
+-- create table Student_info (
+-- name varchar(255),
+-- age int,
+-- fathername varchar(255)
+-- )
+-- show tables;
+-- select * from Student_info;
+
+-- create table Transcation (
+-- transaction_status boolean,
+-- trascation_amount int
+-- )
+-- select * from Transcation ;
+-- select trascation_amount from Transcation ;
+
+
