901dd4bbde5abe34d10429e03cd22a930a3678f1
Trishansh Sahane <133603023+trish-2610@users.noreply.github.com>
2025-01-07 17:12:59 +0530

    SQL_SELECT_UPDATE_DELETE.sql

--- SQL_SELECT_UPDATE_DELETE.sql
+++ SQL_SELECT_UPDATE_DELETE.sql
@@ -1,18 +1,17 @@
 use practice ;
 show tables ;
 
+-- SELECT command used to fetch data
 select * from customer ;
-
 select * from customer where pin_code = 12345 ;
 select * from customer where country in ( "USA" , "India" ) ;
-
 select user_name , pin_code from customer where pin_code = 54321 ;
-
 select distinct pin_code from customer;
 select distinct country from customer where customer_id between 10 and 90 ;
 select distinct pin_code , country from customer ;
 select distinct country , pin_code , customer_id from customer ;
 
+-- UPDATE command used to modify/update data
 update customer set user_name = "Trishansh" where customer_id = 2 ; 
 update customer set user_name = "John Doe" , pin_code = 12345 where customer_id = 2 ;
 
@@ -20,6 +19,7 @@ start transaction;
 update customer set user_name = "ERROR" ;
 rollback ;
 
+-- DELETE command used to delete data from table or delete table itself 
 delete from customer where customer_id = 2 ;
 start transaction ;
 delete from customer ; 
